bfcbb4571da90800679d4b8dd948281e5b3534ae
shourelian <you@example.com>
2019-11-25 16:37:53 -0500

    done

---
+++ week 4/prct/practical1.py
@@ -0,0 +1,7 @@
+n1 = int(input("please enter a number: "))
+n2 = int(input("please enter a number: "))
+
+if n1 > n2:
+    print("the number %d is the gratest" % n1 )
+else:
+    print("the number %d is the gratest" % n2 )
---
+++ week 4/prct/practical2.py
@@ -0,0 +1,7 @@
+a = 23
+b = 23
+
+if a==b:
+    print("the shape is a square")
+else:
+    print("the shape is a reqtangle")
